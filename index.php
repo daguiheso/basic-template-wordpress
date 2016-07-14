46c7111b303a32cec39b5c6329ab5236b9b22563
Daniel H <frontend@reddesignsystems.com>
2016-07-14 15:12:49 -0500

    Show content with the_loop

--- index.php
+++ index.php
@@ -4,10 +4,34 @@
 	<div class="grid max-width">
 		<div class="block grid--item-9">
 			<div class="block__title">
-				Bloque Principal
+				Últimos Trabajos
 			</div>
-			<div class="block__body">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut facilis, totam ratione quaerat. Facilis iste, non alias iusto odit facere ut tempora aperiam aspernatur deserunt est quidem maiores. Obcaecati, voluptatibus!</p>
+			<div class="block__body grid">
+				<?php  
+					if ( have_posts() ) {
+						while (have_posts()) {
+							the_post();
+							?>
+
+							<article class="block grid--item-4">
+								<h2 class="block__title"><?php the_title(); ?></h2>
+								<div class="block__body">
+									<p><?php the_excerpt(); ?></p>
+									<footer>
+										<div>
+											<small><?php the_tags(); ?></small>
+										</div>
+										<div>
+										<b><?php the_author(); ?></b>
+										</div>
+									</footer>
+								</div>
+							</article>
+
+						<?php
+						}
+					}
+				?>
 			</div>
 		</div>
 		<?php get_sidebar(); ?>
